98d57ddddb5113879ec2599afdef7d296455328c
John Knight <bug@knightlabs.com>
2012-06-26 09:18:48 -0400

    Installer.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+DIR=`dirname $0`
+
+if [ ! -d /usr/local/bin ]; then
+  mkdir -p /usr/local/bin
+fi
+if [ ! -d /usr/local/etc ]; then
+  mkdir -p /usr/local/etc
+fi
+
+cp $DIR/service /usr/local/bin/ && cp -R $DIR/services.d /usr/local/etc/
